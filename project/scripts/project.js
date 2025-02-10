5365efe528d60357d6f8bdc8b92588333f3e136c
joakinleiva <mleiva@byupathway.edu>
2025-02-10 04:30:56 -0300

    done with contact

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -5,6 +5,24 @@ function updateDateTime() {
 }
 updateDateTime();
 
+// Map
+var map = L.map('map').setView([-29.9058174, -71.2501308], 12); // Coordenadas de La Serena, Coquimbo
+
+        // Capa de mapa de OpenStreetMap
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // Marcador
+        L.marker([-29.9058174, -71.2501308]).addTo(map)
+            .bindPopup('La Serena, Coquimbo')
+            .openPopup();
+
+
+
+
+
+
 
 /*
 
